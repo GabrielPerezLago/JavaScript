e7ae5de5b68924ab47743fac5e53d86967aa56b5
Gabriel <gabrielperezlago.dev@gmail.com>
2025-04-22 01:11:17 +0200

    CommitCasa20250422

--- MiniProyectos/ShadowTasks/Middleware/js/script.js
+++ MiniProyectos/ShadowTasks/Middleware/js/script.js
@@ -1,5 +1,42 @@
-const _DATA_ARRAY = [];
+const _TASKS_ARRAY = [];
+let idContador = 1;
 
 function createTask(){
+    const _titulo = document.getElementById('titulo').value;
+    const _descripcion = document.getElementById('descripcion').value;
+    const _prioridad = document.getElementById('prioridad').value;
+
+    if(!_titulo || _titulo === ''){
+        return;
+    }
+
+    if(!_descripcion || _descripcion === ''){
+        return; 
+    }
+
+    if(!_prioridad){
+        return; 
+    }
+
+    _TASKS_ARRAY.push({
+        id: idContador++,
+        nombre:_titulo ,
+        descripcion: _descripcion,
+        prioridad : _prioridad
+    });
+
+    console.log(_TASKS_ARRAY);
+
+    printTask(_TASKS_ARRAY);
+
+}
+
+function printTask(arr){
+    const _contenedor = getElementById('contenedor-tasks');
+    
     
 }
+
+const _createTask = document.getElementById('crear');
+
+_createTask.addEventListener("click", createTask);
