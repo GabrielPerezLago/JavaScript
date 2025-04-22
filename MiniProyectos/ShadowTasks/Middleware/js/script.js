ba92e1792158749a85714de4062e0c08355351c9
Gabriel <gabrielperezlago.dev@gmail.com>
2025-04-22 21:57:18 +0200

    CommitCasa20250422

--- MiniProyectos/ShadowTasks/Middleware/js/script.js
+++ MiniProyectos/ShadowTasks/Middleware/js/script.js
@@ -1,4 +1,4 @@
-const _TASKS_ARRAY = [];
+let _TASKS_ARRAY = [];
 let idContador = 1;
 
 function createTask(){
@@ -35,32 +35,52 @@ function printTask(arr){
 
     const _contenedor = document.getElementById("contenedor-tasks");
     _contenedor.innerHTML = "";
+    _contenedor.className = "d-flex flex-wrap gap-3"
     arr.forEach((tarea) => {
         const cardContainer = document.createElement('div');
-        cardContainer.className = 'card';
-        cardContainer.style = "width: 18rem";
+        cardContainer.className = 'card shadow-sm rounded-3';
 
         const cardBody = document.createElement('div');
         cardBody.className = 'card-body';
+
         const cardTittle = document.createElement("h2");
-        cardTittle.className = 'card-title';
+        cardTittle.className = 'card-title m-4 ';
+
         const cardText = document.createElement('p');
-        cardBody.className = 'card-text';
+        cardBody.className = 'card-text m-3';
+
+        const deleteButton = document.createElement('input');
+
+        const buttonContainaner = document.createElement('div');
+        buttonContainaner.className = "d-flex justify-content-end align-items-center";
+        deleteButton.type = 'button';
+        deleteButton.value = 'Eliminar';
+        deleteButton.className = 'btn btn-sm btn-outline-danger';
+
+
+        deleteButton.addEventListener("click",() => {deleteTasks(tarea.id);});
+
         
-        cardTittle.textContent = tarea.id + "-" + tarea.nombre;
+        cardTittle.textContent = tarea.nombre;
         cardText.textContent = tarea.descripcion;
         
+        buttonContainaner.appendChild(deleteButton);
         cardBody.appendChild(cardTittle);
         cardBody.appendChild(cardText);
+        cardBody.appendChild(buttonContainaner);
         cardContainer.appendChild(cardBody);
         _contenedor.appendChild(cardContainer);
     });
+}
 
-    
-
-
+function deleteTasks(id){
+    _TASKS_ARRAY =_TASKS_ARRAY.filter(t => t.id !== id);
+    printTask(_TASKS_ARRAY);
 }
 
 const _createTask = document.getElementById('crear');
 
-_createTask.addEventListener("click", createTask);
+_createTask.addEventListener("click", (event) => {
+    event.preventDefault();
+    createTask();
+});
